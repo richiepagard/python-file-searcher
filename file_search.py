8278793d63e42de31d7aaa1cf73d04ab7eaf75d3
richie <richiepagard@gmail.com>
2026-07-09 00:20:03 -0400

    feat; Implement the `main` funtion to handle user inputs, initial threads, functions calling, and exceptions.

--- file_search.py
+++ file_search.py
@@ -20,7 +20,7 @@ from threads_conf import FileSearchThread
 
 
 # Set logger
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('FILE SEARCHER')
 base_logger(logger)
 
 lock = Lock()
@@ -46,9 +46,11 @@ def file_search(directory: str, target_file: str) -> list:
         if target_file in file:
             with lock:
                 matches.append(file_path)
-                logger.info('Found match: %s', file_path)
+                logger.debug('Found match: %s', file_path)
 
         elif isdir(file_path):
+            logger.debug('Entering subdirectory: %s', file_path)
+
             # Create a new thread for searching in the subdirectory
             search_thread = FileSearchThread(
                 target=file_search,
@@ -57,3 +59,48 @@ def file_search(directory: str, target_file: str) -> list:
             )
             search_thread.start()
             search_thread.join()  # Wait for the thread to finish
+
+
+def main() -> None:
+    """
+    Main function to initiate the file search.
+
+    This function sets up the initial parameters and starts the search process.
+    """
+    try:
+        directory = input("Enter the directory to search in: ")
+        target_file = input("Enter the target file name with extension to search for: ")
+
+        file_search_thread = FileSearchThread(
+            target=file_search,
+            directory=directory,
+            target_file=target_file
+        )
+        file_search_thread.start()
+        file_search_thread.join()  # Wait for the search thread to finish
+
+        if matches:
+            logger.info('Search completed. Matches found:')
+            for match in matches:
+                logger.info('Matched: %s', match)
+        else:
+            logger.info('Search completed. No matches found.')
+
+    except EOFError:
+        logger.error('Input error: No input provided.')
+        # Re-raise the exception to allow the program to terminate gracefully
+        raise
+
+    except KeyboardInterrupt:
+        logger.warning('Search interrupted by user.')
+        # Re-raise the exception to allow the program to terminate gracefully
+        raise
+
+    except Exception as e:
+        logger.error('An unexpected error occurred: %s', str(e))
+        # Re-raise the exception to allow the program to terminate gracefully
+        raise
+
+
+if __name__ == "__main__":
+    main()
